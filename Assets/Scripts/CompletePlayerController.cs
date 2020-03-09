bced8a836244adf631aea8386a6a12a298613f42
Tgl70 <bruceketta70@yahoo.it>
2020-03-09 13:21:00 +0000

    Room

---
+++ Assets/Scripts/CompletePlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CompletePlayerController : MonoBehaviour
+{
+
+    public float speed;                //Floating point variable to store the player's movement speed.
+
+    private GameObject rb3d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb3d = GetComponent<GameObject>();
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        rb3d.AddForce(movement * speed);
+    }
+}
